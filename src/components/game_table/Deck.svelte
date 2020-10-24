0b2a70f1f38416bdc861740d4e2627676637e2f9
Simone Pierazzini <makeroo@gmail.com>
2020-10-24 06:27:32 +0200

    show player deck

--- src/components/game_table/Deck.svelte
+++ src/components/game_table/Deck.svelte
@@ -1,1 +1,14 @@
-<div>deck todo</div>
+<script>
+    import { myDeck } from '../../services/game_service';
+    import { _ } from "svelte-i18n";
+    import { cardI18nName } from '../../services/be_client';
+</script>
+
+
+<div>
+    <ul>
+        {#each $myDeck as card}
+            <li>{$_(cardI18nName(card))}</li>
+        {/each}
+    </ul>
+</div>
