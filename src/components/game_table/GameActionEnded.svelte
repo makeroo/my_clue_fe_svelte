8cb7fd2e31db48780015c0b053f7aec4a81568ff
Simone Pierazzini <makeroo@gmail.com>
2020-11-18 21:53:09 +0100

    show the solution when the game ends

--- src/components/game_table/GameActionEnded.svelte
+++ src/components/game_table/GameActionEnded.svelte
@@ -1,3 +1,14 @@
+<script>
+    import { _ } from "svelte-i18n";
+
+    import { cardToCharacter, cardToRoom, cardToWeapon } from "../../services/be_client";
+    import { currentGameSolution } from "../../services/game_service";
+</script>
+
 <div>
-    todo: game ended
+    <p>{$_('game.end.title')}</p>
+    <p>{$_('game.end.solution')}</p>
+    <p>{$_(`room.${cardToRoom($currentGameSolution.room)}`)}</p>
+    <p>{$_(`weapon.${cardToWeapon($currentGameSolution.weapon)}`)}</p>
+    <p>{$_(`character.${cardToCharacter($currentGameSolution.character)}`)}</p>
 </div>
