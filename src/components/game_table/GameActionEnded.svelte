fe0cf659bcf0aa75b05d20d29f8f362264996341
Simone Pierazzini <s.pierazzini@nextworks.it>
2020-11-21 15:44:53 +0100

    duplicate msg

--- src/components/game_table/GameActionEnded.svelte
+++ src/components/game_table/GameActionEnded.svelte
@@ -6,7 +6,6 @@
 </script>
 
 <div>
-    <p>{$_('game.end.title')}</p>
     <p>{$_('game.end.solution')}</p>
     <p>{$_(`card.${cardToRoom($currentGameSolution.room)}`)}</p>
     <p>{$_(`card.${cardToWeapon($currentGameSolution.weapon)}`)}</p>
