8a4a1f698797e3d0ed5b339a0f2bdd9f637d7508
Simone Pierazzini <makeroo@gmail.com>
2020-12-06 06:29:08 +0100

    game config restyled

--- src/components/ConfigureGame.svelte
+++ src/components/ConfigureGame.svelte
@@ -5,6 +5,7 @@
     import { currentGame, myPlayerId, playerCharacter, turnSequence } from '../services/game_service.js';
     import { Characters } from '../services/my_clue_api.js';
     import { key, playersWithoutCharacter } from '../services/game_service.js';
+    import BigButton from './bricks/BigButton.svelte';
 
     import CharacterSelection from './config_game/CharacterSelection.svelte';
     import ConfiguringPlayer from './config_game/ConfiguringPlayer.svelte';
@@ -21,51 +22,73 @@
         gameService.voteStart().then(() => {
             votedStart = true;
         }).catch((error) => {
-            // TODO: show error
+            window.pushToast($_(`error.${error.error}`));
         })
     }
 
 </script>
 
-<h1>{$_('app.name')}</h1>
+<div class="main-grid">
 
-<h2>{$_('config_game.title')}</h2>
+    <h1 class="logo main-row">{$_('app.name')}</h1>
 
-<p>{$_('config_game.share_game_code')}<span>{$currentGame}</span></p>
+    <h2 class="main-row">{$_('config_game.title')}</h2>
 
-<div>
-    <p>{$_('config_game.select_player')}</p>
+    <p class="main-row">
+        <span>{$_('config_game.share_game_code')}</span>
+        <span class="code">{$currentGame}</span>
+    </p>
 
-    <ul>
-        {#each Characters as character }
-            <li>
+    <div class="main-row">
+        <p>{$_('config_game.select_player')}</p>
+
+        <div class="character-grid">
+            {#each Characters as character }
                 <CharacterSelection character={character}/>
-            </li>
-        {/each}
-    </ul>
-</div>
-
-<div>
-    {#if $turnSequence.length < 2}
-        <p>{$_('config_game.not_enough_players')}</p>
-    {:else}
-        <input type=submit disabled={!$myCharacter || votedStart} value="{$_('config_game.start')}" on:click={voteStart}>
-    {/if}
-</div>
-
-{#if $playersWithoutCharacter.length > 0 }
-    <div>
-        <p>{$_('config_game.players_without_character')}</p>
-        {#each $turnSequence as playerId }
-            <ConfiguringPlayer playerId={playerId} hideIfReady={true}/>
-        {/each}
+            {/each}
+        </div>
     </div>
-{/if}
 
-{#if $myCharacter && $turnSequence.length >= 2}
-    {#if votedStart}
-        <p>{$_('config_game.waiting')}</p>
-    {:else}
-        <p>{$_('config_game.press_start')}</p>
+    <div class="main-row">
+        {#if $turnSequence.length < 2}
+            <p>{$_('config_game.not_enough_players')}</p>
+        {:else}
+            <BigButton disabled={!$myCharacter || votedStart} on:click={voteStart}>{$_('config_game.start')}</BigButton>
+        {/if}
+    </div>
+
+    {#if $playersWithoutCharacter.length > 0 }
+        <div class="main-row">
+            <p>{$_('config_game.players_without_character')}</p>
+            {#each $turnSequence as playerId }
+                <ConfiguringPlayer playerId={playerId} hideIfReady={true}/>
+            {/each}
+        </div>
     {/if}
-{/if}
+
+    {#if $myCharacter && $turnSequence.length >= 2}
+        {#if votedStart}
+            <p class="main-row">{$_('config_game.waiting')}</p>
+        {:else}
+            <p class="main-row">{$_('config_game.press_start')}</p>
+        {/if}
+    {/if}
+
+</div>
+
+
+<style>
+    .code {
+        color: #C0BCC7;
+        font-family: monospace;
+        font-weight: 800;
+        font-size: 26px;
+    }
+
+    .character-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        grid-gap: 1em;
+        margin: 1em 0;
+    }
+</style>
--- src/components/config_game/CharacterSelection.svelte
+++ src/components/config_game/CharacterSelection.svelte
@@ -1,7 +1,8 @@
 <script>
     import { _ } from "svelte-i18n";
     import { getContext } from 'svelte';
-    import { key, characterPlayer } from "../../services/game_service.js";
+    import { key, characterPlayer, myPlayerId } from "../../services/game_service.js";
+    import BigButton from '../bricks/BigButton.svelte';
 
     import ConfiguringPlayer from './ConfiguringPlayer.svelte';
 
@@ -13,18 +14,37 @@
 
     function selectCharacter() {
         gameService.selectCharacter(character).catch((error) => {
-            console.log("azz". error);
-            // TODO: show error
+            window.pushToast($_(`error.${error.error}`));
         });
     }
 
+    // BigButton has 3 states: available->-, mine->valid, not available->disabled
+    let buttonState;
+
+    $: buttonState = $myPlayer === $myPlayerId ? "valid" : $myPlayer === null ? "" : "disabled";
+
 </script>
 
-<div>
-    <button type=button on:click={selectCharacter}>{$_(`card.${character}`)}</button>
-    {#if $myPlayer === null }
-        <p>{$_('config_game.character_available')}</p>
-    {:else}
-        <ConfiguringPlayer playerId={$myPlayer} hideIfReady={false}/>
-    {/if}
+<div class="container">
+    <BigButton class={buttonState} on:click={selectCharacter}>{$_(`card.${character}`)}</BigButton>
+    <div class="name">
+        {#if $myPlayer === null }
+            <p>{$_('config_game.character_available')}</p>
+        {:else}
+            <ConfiguringPlayer playerId={$myPlayer} hideIfReady={false}/>
+        {/if}
+    </div>
 </div>
+
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .name {
+        margin-top: .5em;
+    }
+</style>
